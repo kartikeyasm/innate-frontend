fff69bb291e620a995b64782a785ff2b1f077715
kartikeyasm <kartikeyasm16@gmail.com>
2025-02-14 23:54:09 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <navbar></navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <>
+        <div>
+            
+        </div>
+    </>
+  )
+}
+
+export default navbar
